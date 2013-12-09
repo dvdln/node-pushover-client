edef3029b1848ba1ed8ab1510d628f3ec5ed4f28
David Lane <dvdln@live.com>
2013-12-09 17:54:01 -0500

    Add unit test

---
+++ test/mock/https.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var https = require('https');
+
+var response = {
+  content: ''
+};
+
+var request = {
+  encoding: '',
+  data: '',
+  cb: {}
+};
+
+https.request = function (options, callback) {
+  callback({
+    setEncoding: function (enc) {
+      request.encoding = enc;
+    },
+    on: function (event, callback) {
+      request.cb[event] = callback;
+    }
+  });
+
+  return {
+    write: function (data) {
+      request.data = data;
+    },
+    end: function () {
+      request.cb.data(response.content);
+    }
+  };
+};
+
+https.mock = {
+  reset: function () {
+    response = {
+      content: ''
+    };
+
+    request = {
+      encoding: '',
+      data: '',
+      cb: {}
+    };
+  },
+
+  getRequest: function () {
+    return request;
+  },
+
+  setResponse: function (content) {
+    response.content = content;
+  }
+};
+
+
+module.exports = https;
