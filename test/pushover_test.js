1c72c5bbf5eada6fbfd45e2fe7f9580be00eabcb
David Lane <dvdln@live.com>
2013-12-10 02:02:40 -0500

    Update tests

--- test/pushover_test.js
+++ test/pushover_test.js
@@ -3,6 +3,9 @@
 var https = require('./mock/https'),
     Pushover = require('../lib/pushover');
 
+process.env.PUSHOVER_DEFAULT_TOKEN = 'ENV_TOKEN';
+process.env.PUSHOVER_DEFAULT_USER = 'ENV_USER';
+
 module.exports = {
   setUp: function (callback) {
     https.mock.reset();
@@ -15,7 +18,7 @@ module.exports = {
     callback();
   },
 
-  testPushover: function (test) {
+  testPush: function (test) {
     var push = new Pushover({
       token: 'TOKEN',
       user: 'USER',
@@ -34,9 +37,62 @@ module.exports = {
 
         test.equal(req.encoding, 'utf8');
         test.deepEqual(res, { status: 1 });
-
         test.equal(req.data, 'token=TOKEN&user=USER&title=TITLE&' +
           'message=MESSAGE&timestamp=123&priority=-1\n');
+
+        test.done();
+      }
+    });
+  },
+
+  testMinimal: function (test) {
+    (new Pushover()).send({
+      message: 'MESSAGE',
+      timestamp: null,
+
+      done: function (res) {
+        var req = https.mock.getRequest();
+
+        test.deepEqual(res, { status: 1 });
+        test.equal(req.data, 'token=ENV_TOKEN&user=ENV_USER&message=MESSAGE\n');
+
+        test.done();
+      }
+    });
+  },
+
+  testEmergencyPriority: function (test) {
+    (new Pushover()).send({
+      message: 'MESSAGE',
+      timestamp: null,
+      priority: 2,
+      retry: 30,
+      expire: 3600,
+
+      done: function (res) {
+        var req = https.mock.getRequest();
+
+        test.deepEqual(res, { status: 1 });
+        test.equal(req.data, 'token=ENV_TOKEN&user=ENV_USER&message=MESSAGE&' +
+            'priority=2&expire=3600&retry=30\n');
+
+        test.done();
+      }
+    });
+  },
+
+  testEmergencyPriorityBadData: function (test) {
+    (new Pushover()).send({
+      message: 'MESSAGE',
+      timestamp: null,
+      priority: 2,
+
+      done: function (res) {
+        var req = https.mock.getRequest();
+
+        test.ok(res.error.length > 0);
+        test.equal(res.status, null);
+        test.equal(req.data, '');
 
         test.done();
       }
